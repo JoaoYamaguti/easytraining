faab6a0b81f269d9dcef1bcfa53a015d9678936b
JoaoYamaguti <105678854+JoaoYamaguti@users.noreply.github.com>
2025-01-10 16:34:02 -0300

    fix: calc % on progressivebar

--- src/app/ui/components/progressiveBarSession/index.tsx
+++ src/app/ui/components/progressiveBarSession/index.tsx
@@ -12,7 +12,7 @@ export default function ProgressiveBarSession(props: IProgressiveBarProps) {
         <section className="progressiveBarSession">
             <h3></h3>
             <div className="ProgrssiveShell">
-                <div className="ProgrssiveFill" style={"inline-size:" + (sessions / (sessions * 12)) + "%;"}></div>
+                <div className="ProgrssiveFill" style={"inline-size:" + ((sessions / (sessions * 12)) * 100) + "%;"}></div>
             </div>
             <p>{sessions} / {sessions * 12}</p>
         </section>
